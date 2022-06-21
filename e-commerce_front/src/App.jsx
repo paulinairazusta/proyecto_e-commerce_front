15f6877a31fa6765b0039bc8e4a013b05a579734
Santiago Fantoni <fantonisantiago@gmail.com>
2022-06-21 20:47:57 -0300

    Banner y botones

--- e-commerce_front/src/App.jsx
+++ e-commerce_front/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import Banner from "./components/banner/Banner";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavComp from "./components/navbar/Nav";
 
 function App() {
 	return (
 		<div className='App'>
+			<NavComp />
 			<Banner />
 		</div>
 	);
--- e-commerce_front/src/components/banner/Banner.jsx
+++ e-commerce_front/src/components/banner/Banner.jsx
@@ -2,9 +2,24 @@ import "./banner.css";
 function Banner() {
 	return (
 		<div className='bg-banner'>
-			<div className='title'>
-				<h2>Sweet Bites</h2>
+			<div className='title-container'>
+				<span>Sweet bites for you</span>
 				<h1>TASTEFUL</h1>
+				<img
+					src='https://dolcino.qodeinteractive.com/wp-content/uploads/2018/10/h1-slide-1-img-1.png'
+					alt=''
+				/>
+				<p>
+					It´s allways good time for sweets! Made with care and prepared with
+					love, our sweets are all you might need to make every day special.
+					Find your favorite now.
+				</p>
+				<a className='a-discover' href='#'>
+					Discover
+				</a>
+				<a className='a-shop' href='#'>
+					Shop now
+				</a>
 			</div>
 		</div>
 	);
