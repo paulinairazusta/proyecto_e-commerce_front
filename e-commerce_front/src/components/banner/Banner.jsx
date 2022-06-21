ac5532d36e5d3f5b41d7eaec1504167594219967
Santiago Fantoni <fantonisantiago@gmail.com>
2022-06-21 17:05:03 -0300

    banner y fuentes

---
+++ e-commerce_front/src/components/banner/Banner.jsx
@@ -0,0 +1,13 @@
+import "./banner.css";
+function Banner() {
+	return (
+		<div className='bg-banner'>
+			<div className='title'>
+				<h2>Sweet Bites</h2>
+				<h1>TASTEFUL</h1>
+			</div>
+		</div>
+	);
+}
+
+export default Banner;
